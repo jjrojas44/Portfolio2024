9c943fbe48121ad48cbb15594421ef31de9a1e8a
jjrojas44 <95251262+jjrojas44@users.noreply.github.com>
2026-05-12 14:28:37 -0400

    project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1204,3 +1204,20 @@ function changeWebImage(imageSrc, title, description) {
     `;
   }
 }
+
+function changeProductImage(imageSrc, title, description) {
+  const mainImage = document.getElementById("productDisplayedImage");
+  const caption = document.querySelector(".hover-caption");
+
+  if (mainImage) {
+    mainImage.src = imageSrc;
+    mainImage.alt = title;
+  }
+
+  if (caption) {
+    caption.innerHTML = `
+      <h4>${title}</h4>
+      <p>${description}</p>
+    `;
+  }
+}
